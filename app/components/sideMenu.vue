ab6101a87c3aa94c9059e96f6ff3c64756bf5439
KaitlynRossi <kaitirossi@gmail.com>
2025-09-26 21:00:55 -0400

    Add navbar, side menu, and student search bar (Note: I did not add any functionality to the search bar)

---
+++ app/components/sideMenu.vue
@@ -0,0 +1,50 @@
+
+<script setup>
+    defineProps({
+        isOpen: Boolean
+    })
+</script>
+
+<template>
+     <Transition name="slide">
+        <div v-if="isOpen" class="fixed left-0 h-screen w-full md:w-1/2 lg:w-1/4 bg-[#2e777e] drop-shadow-lg sideMenuDropShadow">
+            <ul class="pt-16">
+                <!--The below are placeholder items, the classes should not be replaced as that's what gives it the style, but names and links can be added inside the div-->
+                <div class="p-2 border-b border-white border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-5 text-lg text-[#e2fafc]">Section 1 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 1 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 2 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 3 Placeholder</li></div>
+
+                <div class="p-2 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-5  text-lg text-[#e2fafc]">Section 2 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 1 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 2 Placeholder</li></div>
+                <div class="p-1 border-b border-white  border-1 flex items-center justify-start hover:bg-[#205a5f] cursor-pointer"><li class="indent-7 text-[#e2fafc]">Subsection 3 Placeholder</li></div>
+            </ul>
+        </div>
+  </Transition>
+</template>
+
+<!--This is the style to make the side menu slide left and right on click-->
+<style>
+    .slide-enter-from {
+        transform: translateX(-100%);
+    }
+
+    .slide-enter-to {
+        transform: translateX(0);
+    }
+    .slide-leave-from {
+        transform: translateX(0);
+    }
+    .slide-leave-to {
+        transform: translateX(-100%);
+    }
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: transform 0.5s ease-in-out;
+    }
+    .sideMenuDropShadow{
+        box-shadow: 0 1px 5px #205a5f;
+        z-index: 999;
+    }
+</style>
---
+++ app/components/topNavbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import SideMenu from '~/components/sideMenu.vue'
+
+
+    const isSidebarOpen = ref(false)
+
+    const toggleSidebar = () => {
+        isSidebarOpen.value = !isSidebarOpen.value
+    }
+</script>
+
+<template>
+    <!-- This creates the top navigation bar -->
+    <div class="fixed w-screen h-16 bg-[#2e777e] flex items-center justify-end justify-between navBarDropShadow border-b border-1 border-[#16383b]" title="Menu">
+        <Icon @click="toggleSidebar" name="arcticons:hamburger-menu" class="h-10 w-10 text-[#e2fafc] cursor-pointer ml-2"></Icon>
+        <h1 class="text-[#e2fafc] text-center">FAST Track Academy Student Progress Tracking</h1>
+        <Icon name="material-symbols-light:logout" class="h-10 w-10 text-[#e2fafc] cursor-pointer mr-2" title="Logout" />
+    </div>
+
+    <!-- Displays side menu if hamburger menu is clicked-->
+    <SideMenu :isOpen="isSidebarOpen" @close="isSidebarOpen = false"/>
+    <mainDash></mainDash>
+  </template>
+
+<style>
+.navBarDropShadow {
+    box-shadow: 0 1px 5px #16383b;
+    z-index: 1000;
+}
+</style>
