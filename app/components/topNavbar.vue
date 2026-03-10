73b9b01a45e4fbbf2330c4bc7335c7f46e82cd9d
AhaduChere <ahaduchere546@gmail.com>
2026-03-10 15:03:56 -0400

    fixing isarchived on student management

--- app/components/topNavbar.vue
+++ app/components/topNavbar.vue
@@ -37,7 +37,7 @@ onBeforeUnmount(() => {
     class="fixed w-screen h-16 bg-[#2e777e] flex items-center justify-between navBarDropShadow border-b border-1 border-[#16383b]"
     title="Menu">
     <Icon id="icon" name="arcticons:hamburger-menu" class="h-10 w-10 text-[#e2fafc] cursor-pointer ml-2" @click="toggleSidebar"></Icon>
-    <img class="h-full" src="/possibleFASTTrackLogo2.png">
+    <img class="h-full mb-3" src="/possibleFASTTrackLogo2.png">
     <!--<h1 class="text-[#e2fafc] text-center">FAST Track Academy Student Progress Tracking</h1>-->
  <div class="h-full flex items-center justify-end pr-4 text-[#e2fafc]">
       <span class="text-sm font-mono tracking-wide">{{ time }}</span>
